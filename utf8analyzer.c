7889cfbf1ccd800380d250ba9ca6d413335f6473
Abhilekh Meda <abhilekh.meda@gmail.com>
2025-07-09 13:24:35 -0700

    function 1,2,3

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -3,6 +3,39 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
+bool function1(char str[]){
+        bool holder = true;
+        for(int i = 0; str[i] != '\0'; i++){
+                if((uint8_t)str[i] > 127){
+                        holder = false;
+                }
+        }
+        bool result1 = holder;
+        return result1;
+}
+void function2(char str[]){
+        for(int i = 0; str[i] != '\0'; i++){
+                if(((uint8_t)str[i] >= 97) && ((uint8_t)str[i] <= 122)){
+                        str[i] -= 32;
+                }
+        }
+}
+        //the input string is changed
+int function3(char str[]){
+        int i = 0;
+        while(str[i] != '\0'){
+                if((str[i] & 0b10000000) == 0b00000000){
+                        i++;
+                } else if((str[i] & 0b11100000) == 0b11000000){
+                        i+=2;
+                } else if((str[i] & 0b11110000) == 0b11100000){
+                        i+=3;
+                } else if((str[i] & 0b11111000) == 0b11110000){
+                        i+=4;
+                }
+        }
+        return i;
+}
 
 void function7(const char *s) { 
 	int end = 0; 
