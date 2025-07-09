2f5d0ba45b144b181017b19335c1e0f2b5693c8d
jsmoe900 <jsmoe@ucsd.edu>
2025-07-09 13:30:22 -0700

    Update utf8analyzer.c

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -112,29 +112,28 @@ void function6(char str[]){
         }
         puts("\n");
 }
-void function7(const char *s) { 
-	int end = 0; 
-	for (int i = 0; s[end]  != '\0' && i < 6; i++) {
-		unsigned char c = (unsigned char)s[i]; 
-		int len; 
+void function7(char str[]) {
+        int end = 0;
+        for (int i = 0; str[end]  != '\0' && i < 6; i++) {
+                unsigned char c = (unsigned char)str[i];
+                int len;
 
-		if ((c & 0x80) == 0x00){
-			len = 1; 
-		} 
-		else if ((c & 0xE0) == 0xC0){
-			len = 2;
-		}
-		else if ((c & 0xF0) == 0xE0) {
-			len = 3; 
-		}
-		else {
-			len = 4;
-		}
-		end += len;  
-	}
-	printf("%.*s\n", end, s); 
+                if ((c & 0x80) == 0x00){
+                        len = 1;
+                }
+                else if ((c & 0xE0) == 0xC0){
+                        len = 2;
+                }
+                else if ((c & 0xF0) == 0xE0) {
+                        len = 3;
+                }
+                else {
+                        len = 4;
+                }
+                end += len;
+        }
+        printf("%.*s\n", end, str);
 }
-
 
 
 int main(int argc, char *argv[]) {
